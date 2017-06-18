ad405a89f3746c107803f1ffa491a4ff8e870b5b
godfried <you_sombo@hotmail.fr>
2017-06-18 18:07:07 +0200

    First Node tutorial

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // const obj = {top: 0, bottom: 1};
+  // res.json(req.query);
+  // res.send('Hey! It works!');
+  res.render('hi', {
+    name: req.query.name || 'Foobar'
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // res.send('Hey! It works too!');
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
